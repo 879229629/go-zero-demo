16199f2fbf7632928004811685b3c4daa492c40f
zenggang <zg879229629@qq.com>
2022-12-04 11:44:50 +0800

    first commit

---
+++ mall/order/api/internal/logic/getorderlogic.go
@@ -0,0 +1,40 @@
+package logic
+
+import (
+	"context"
+	"go-zero-demo/mall/order/api/internal/svc"
+	"go-zero-demo/mall/order/api/internal/types"
+	"go-zero-demo/mall/user/rpc/types/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetOrderLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewGetOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetOrderLogic {
+	return &GetOrderLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *GetOrderLogic) GetOrder(req *types.OrderReq) (resp *types.OrderReply, err error) {
+	user, err := l.svcCtx.UserRpc.GetUser(l.ctx, &user.IdRequest{
+		Id: req.Id,
+	})
+	if err != nil {
+		return nil, err
+	}
+	l.Infof("user: %v", user)
+
+	return &types.OrderReply{
+		Id:   user.GetId(),
+		Name: user.GetName(),
+	}, nil
+
+}
